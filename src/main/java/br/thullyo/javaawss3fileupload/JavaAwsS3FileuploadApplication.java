d7d161cb45d11939bcc78390f92ede03cbdfc058
Thullyoo <thullyofilho@gmail.com>
2024-10-02 13:30:29 -0300

    init: first commit

---
+++ src/main/java/br/thullyo/javaawss3fileupload/JavaAwsS3FileuploadApplication.java
@@ -0,0 +1,13 @@
+package br.thullyo.javaawss3fileupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAwsS3FileuploadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaAwsS3FileuploadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/thullyo/javaawss3fileupload/JavaAwsS3FileuploadApplicationTests.java
@@ -0,0 +1,13 @@
+package br.thullyo.javaawss3fileupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAwsS3FileuploadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
