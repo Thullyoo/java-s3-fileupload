04d93b015f8b04243464a2b69c657045670e3cb9
Thullyoo <thullyofilho@gmail.com>
2024-10-03 14:30:01 -0300

    fix: adição de anotações necessárias

--- src/main/java/br/thullyo/javaawss3fileupload/config/S3Config.java
+++ src/main/java/br/thullyo/javaawss3fileupload/config/S3Config.java
@@ -1,6 +1,7 @@
 package br.thullyo.javaawss3fileupload.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
@@ -19,6 +20,7 @@ public class S3Config {
     @Value("${aws.secretKey}")
     private String secretKey;
 
+    @Bean
     public S3Client s3Client(){
         AwsBasicCredentials awsBasicCredentials = AwsBasicCredentials.create(accessKeyId, secretKey);
 
--- src/main/java/br/thullyo/javaawss3fileupload/controller/FileUploadController.java
+++ src/main/java/br/thullyo/javaawss3fileupload/controller/FileUploadController.java
@@ -2,8 +2,7 @@ package br.thullyo.javaawss3fileupload.controller;
 
 import br.thullyo.javaawss3fileupload.service.FileUploadService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +17,8 @@ public class FileUploadController {
     @Autowired
     private FileUploadService fileUploadService;
 
-    @PostMapping
+
+    @PostMapping(consumes = "multipart/form-data")
     public ResponseEntity<Void> uploadFile(@RequestParam("file")MultipartFile file){
         fileUploadService.uploadFile(file);
         return ResponseEntity.ok().body(null);
